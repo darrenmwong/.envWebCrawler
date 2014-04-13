eef17a8b2225cb10f30a96ed66be307b6cb6cfbd
darrenmwong <darrenwong06@gmail.com>
2014-04-12 17:04:31 -0700

    generated rails application

---
+++ envWebCrawl/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnvWebCrawl::Application.config.secret_key_base = '341f9a121c1870e057d9ca4bc84a5d063947ca392974ac106a829c2fa5db61c8f245fb2a5c093af69d27a83e7bd62361afaebcc4f406c71de929aa86286870e3'
