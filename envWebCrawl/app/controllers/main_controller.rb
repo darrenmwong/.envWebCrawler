4c69c2cd6e9c17a169b97c765713b60a5ffa4fc1
darrenmwong <darrenwong06@gmail.com>
2014-06-02 14:35:48 -0700

    added main controller,views, and routes

---
+++ envWebCrawl/app/controllers/main_controller.rb
@@ -0,0 +1,15 @@
+class MainController < ApplicationController
+
+def index
+end
+
+
+def search
+end
+
+
+
+
+
+
+end
--- envWebCrawl/config/routes.rb
+++ envWebCrawl/config/routes.rb
@@ -1,4 +1,10 @@
 EnvWebCrawl::Application.routes.draw do
+    resources :main
+
+root to: "main#index"
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
