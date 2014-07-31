a6e0eda9e5647cb3a15f1d51a4a586c84ee3af56
darrenmwong <darrenwong06@gmail.com>
2014-07-30 17:41:13 -0700

    traverse down to repositories

--- envWebCrawl/app/controllers/main_controller.rb
+++ envWebCrawl/app/controllers/main_controller.rb
@@ -1,14 +1,20 @@
 class MainController < ApplicationController
 
+require 'open-uri'
+
 def index
   
 end
 
 
 def search
+  #Parameters in search box showing github account name
   @link = params[:github]
-  @github_doc = Nokogiri::HTML(Typhoeus.get(@link).body)
-  puts @github_doc
+  gitLink = "https://www.github.com/#{@link}?tab=repositories"
+  #Web Crawling Getting HTML back from Github
+  github_doc = Nokogiri::HTML(open(gitLink))
+  #Search specific class traverse down to repository links
+  @gitRepo = github_doc.search('.repo-tab')
   # Hardworker.perform_async(@link, 1)
   render "/main/results"
 end
